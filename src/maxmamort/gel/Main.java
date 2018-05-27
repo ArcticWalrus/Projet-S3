ce714a94d849d6821a313ac2da2d3e5c5016065b
maxmamort <maximbolduc74@gmail.com>
2018-05-27 14:47:44 -0400

    refractored classes

--- src/maxmamort/gel/Main.java
+++ src/maxmamort/gel/Main.java
@@ -12,109 +12,23 @@ import java.util.Date;
 
 public class Main {
 
-    public static int addInput(String inputName, double defaultValue, int sensorType) {
-        int returnValue = -1;
-        String sql = "INSERT INTO public.intinput VALUES(DEFAULT, '" + inputName + "' , '" + defaultValue + "','" + sensorType + "') RETURNING serintinput;";
-        dbAccess db = new dbAccess();
-        returnValue = db.insertGetIdQuery(sql, "serintinput");
-        db.closeConnection();
-        return returnValue;
-    }
 
-    public static int createInputGroup(int[] inputIds) {
-        int returnValue = -1;
-        String sql = "INSERT INTO public.inputgroup (serinputgroup, conditiongroup, inputid, ordre) VALUES(DEFAULT, '" + "-1" + "' , '" + inputIds[0] + "',0) RETURNING serinputgroup;";
-        dbAccess db = new dbAccess();
-        returnValue = db.insertGetIdQuery(sql, "serinputgroup");
-        //TODO remove update and replace with something that tells sql to put id in conditiongroup
-        sql = "UPDATE public.inputgroup SET conditiongroup = " + returnValue + " WHERE serinputgroup = " + returnValue;
-        db.updateQuery(sql);
-        //   System.out.println(returnValue);
-        for (int i = 1; i < inputIds.length; i++) {
-            //TODO merge sql queries with array rather than each individual call
-            sql = "INSERT INTO public.inputgroup (serinputgroup, conditiongroup, inputid, ordre) VALUES(DEFAULT, '" + returnValue + "' , '" + inputIds[i] + "','" + i + "');";
-            db.insertQuery(sql);
-        }
-        db.closeConnection();
-        return returnValue;
-    }
-
-    public static int createCondition(int inputGroup, int outputGroup, int conditionType) {
-        int returnValue = -1;
-        dbAccess db = new dbAccess();
-        String sql = "INSERT INTO public.conditions (serconditions, inputgroup, outputgroup, operation) VALUES(DEFAULT , '" + inputGroup + "','" + outputGroup + "','" + conditionType + "' ) RETURNING serconditions;";
-        returnValue = db.insertGetIdQuery(sql, "setconditions");
-        db.closeConnection();
-        return returnValue;
-    }
-
-    public static JSONArray getConditionsAndInputs(int inputId) {
-        JSONArray json = null;
-
-        ArrayList<JSONArray> conditions = new ArrayList<>();
-        ArrayList<JSONArray> inputGroups = new ArrayList<>();
-        ArrayList<JSONArray> outputs = new ArrayList<>();
-
-        dbAccess db = new dbAccess();
-        json = db.selectQuery("SELECT * FROM public.inputgroup WHERE inputid = '" + inputId + "'");
-        System.out.println("Input group for given input is: " + json.toString());
-        List<Integer> conditionGroupId = new ArrayList<>();
-
-        //Get conditions assosciated with input
-        for (int i = 0; i < json.length(); i++) {
-            JSONObject obj = json.getJSONObject(i);
-            conditionGroupId.add(obj.getInt("conditiongroup"));// input condition id addition
-            // System.out.println("Condition ID: " + conditionGroupId.get(i));
-            conditions.add(db.selectQuery("SELECT * FROM public.conditions WHERE inputgroup = " + conditionGroupId.get(i)));
-        }
-        //  for(int i = 0; i < conditions.size(); i++ ){
-        //     System.out.println("data is: " + conditions.get(i).toString());
-        //}
-        //   System.out.println(conditions.toString() + "\n\n");
-        json = Utils.getMergeJson(conditions);
-
-        //Get ouput group
-        for (int i = 0; i < json.length(); i++) {
-            JSONObject obj = json.getJSONObject(i);
-            conditionGroupId.add(obj.getInt("outputgroup"));
-            System.out.println("Output group id is: " + obj.getInt("outputgroup"));
-        }
-
-        //Get inputGroups with conditions
-        for (int i = 0; i < conditionGroupId.size(); i++) {
-            inputGroups.add(db.selectQuery("SELECT * FROM public.inputgroup WHERE conditiongroup = " + conditionGroupId.get(i)));
-        }
-        json = Utils.getMergeJson(inputGroups);
-
-        //Get inputs associated with groupid
-        for (int i = 0; i < json.length(); i++) {
-            JSONObject obj = json.getJSONObject(i);
-            int id = obj.getInt("inputid");
-            outputs.add(db.selectQuery("SELECT * FROM public.intinput WHERE serintinput = " + id));
-        }
-
-        //Combine all elements
-        conditions.addAll(inputGroups);
-        conditions.addAll(outputs);
-        json = Utils.getMergeJson(conditions);
-        return json;
-    }
 
     public static void main(String[] args) {
-        int temp = addInput("Test input", 22.5, 0);
-        int temp2 = addInput("Input2", 21.2, 0);
+        int temp = persistantLayer.addInput("Test input", 22.5, 0);
+        int temp2 = persistantLayer.addInput("Input2", 21.2, 0);
 
-        int inputGroup = createInputGroup(new int[]{temp, temp2});
+        int inputGroup = persistantLayer.createInputGroup(new int[]{temp, temp2});
 
-        int temp3 = addInput("output1", 1.3, 1);
-        int outputGroup = createInputGroup(new int[]{temp3});
+        int temp3 = persistantLayer.addInput("output1", 1.3, 1);
+        int outputGroup = persistantLayer.createInputGroup(new int[]{temp3});
 
-        int conditionId = createCondition(inputGroup, outputGroup, 1);
+        int conditionId = persistantLayer.createCondition(inputGroup, outputGroup, 1);
         //  System.out.println(conditionId);
 
         //TODO add column to differentiate input from output
 
-        JSONArray jsonFinal = getConditionsAndInputs(temp);
+        JSONArray jsonFinal = persistantLayer.getConditionsAndInputs(temp);
         for (int i = 0; i < jsonFinal.length(); i++) {
             System.out.println(jsonFinal.get(i).toString());
         }
---
+++ src/maxmamort/gel/persistantLayer.java
@@ -0,0 +1,97 @@
+package maxmamort.gel;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class persistantLayer {
+    public static int addInput(String inputName, double defaultValue, int sensorType) {
+        int returnValue = -1;
+        String sql = "INSERT INTO public.intinput VALUES(DEFAULT, '" + inputName + "' , '" + defaultValue + "','" + sensorType + "') RETURNING serintinput;";
+        dbAccess db = new dbAccess();
+        returnValue = db.insertGetIdQuery(sql, "serintinput");
+        db.closeConnection();
+        return returnValue;
+    }
+
+    public static int createInputGroup(int[] inputIds) {
+        int returnValue = -1;
+        String sql = "INSERT INTO public.inputgroup (serinputgroup, conditiongroup, inputid, ordre) VALUES(DEFAULT, '" + "-1" + "' , '" + inputIds[0] + "',0) RETURNING serinputgroup;";
+        dbAccess db = new dbAccess();
+        returnValue = db.insertGetIdQuery(sql, "serinputgroup");
+        //TODO remove update and replace with something that tells sql to put id in conditiongroup
+        sql = "UPDATE public.inputgroup SET conditiongroup = " + returnValue + " WHERE serinputgroup = " + returnValue;
+        db.updateQuery(sql);
+        //   System.out.println(returnValue);
+        for (int i = 1; i < inputIds.length; i++) {
+            //TODO merge sql queries with array rather than each individual call
+            sql = "INSERT INTO public.inputgroup (serinputgroup, conditiongroup, inputid, ordre) VALUES(DEFAULT, '" + returnValue + "' , '" + inputIds[i] + "','" + i + "');";
+            db.insertQuery(sql);
+        }
+        db.closeConnection();
+        return returnValue;
+    }
+
+    public static int createCondition(int inputGroup, int outputGroup, int conditionType) {
+        int returnValue = -1;
+        dbAccess db = new dbAccess();
+        String sql = "INSERT INTO public.conditions (serconditions, inputgroup, outputgroup, operation) VALUES(DEFAULT , '" + inputGroup + "','" + outputGroup + "','" + conditionType + "' ) RETURNING serconditions;";
+        returnValue = db.insertGetIdQuery(sql, "setconditions");
+        db.closeConnection();
+        return returnValue;
+    }
+
+    public static JSONArray getConditionsAndInputs(int inputId) {
+        JSONArray json = null;
+
+        ArrayList<JSONArray> conditions = new ArrayList<>();
+        ArrayList<JSONArray> inputGroups = new ArrayList<>();
+        ArrayList<JSONArray> outputs = new ArrayList<>();
+
+        dbAccess db = new dbAccess();
+        json = db.selectQuery("SELECT * FROM public.inputgroup WHERE inputid = '" + inputId + "'");
+        System.out.println("Input group for given input is: " + json.toString());
+        List<Integer> conditionGroupId = new ArrayList<>();
+
+        //Get conditions assosciated with input
+        for (int i = 0; i < json.length(); i++) {
+            JSONObject obj = json.getJSONObject(i);
+            conditionGroupId.add(obj.getInt("conditiongroup"));// input condition id addition
+            // System.out.println("Condition ID: " + conditionGroupId.get(i));
+            conditions.add(db.selectQuery("SELECT * FROM public.conditions WHERE inputgroup = " + conditionGroupId.get(i)));
+        }
+        //  for(int i = 0; i < conditions.size(); i++ ){
+        //     System.out.println("data is: " + conditions.get(i).toString());
+        //}
+        //   System.out.println(conditions.toString() + "\n\n");
+        json = Utils.getMergeJson(conditions);
+
+        //Get ouput group
+        for (int i = 0; i < json.length(); i++) {
+            JSONObject obj = json.getJSONObject(i);
+            conditionGroupId.add(obj.getInt("outputgroup"));
+            System.out.println("Output group id is: " + obj.getInt("outputgroup"));
+        }
+
+        //Get inputGroups with conditions
+        for (int i = 0; i < conditionGroupId.size(); i++) {
+            inputGroups.add(db.selectQuery("SELECT * FROM public.inputgroup WHERE conditiongroup = " + conditionGroupId.get(i)));
+        }
+        json = Utils.getMergeJson(inputGroups);
+
+        //Get inputs associated with groupid
+        for (int i = 0; i < json.length(); i++) {
+            JSONObject obj = json.getJSONObject(i);
+            int id = obj.getInt("inputid");
+            outputs.add(db.selectQuery("SELECT * FROM public.intinput WHERE serintinput = " + id));
+        }
+
+        //Combine all elements
+        conditions.addAll(inputGroups);
+        conditions.addAll(outputs);
+        json = Utils.getMergeJson(conditions);
+        return json;
+    }
+}
