65a7c8fa5efefb063f4ef898ea220394a8993331
joshua denis <joshua.denis@hotmail.ca>
2018-05-31 12:06:02 -0400

    Revamped condition sorting method of class LMThread. 3 included logic tests are tested and verified to work so far

--- src/maxmamort/gel/LMThread.java
+++ src/maxmamort/gel/LMThread.java
@@ -5,16 +5,21 @@ package maxmamort.gel;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 
 public class LMThread implements Runnable {
+
+    private static final double TOLERANCE = 0.01;
+
     private int _intInstruction;
     private ArrayList<String> _lstArgs = new ArrayList<String>();
     private boolean _booThreadStop = false;
-    ArrayList<Integer> _lstOutputSerials = new ArrayList<Integer>();
+    ArrayList<JSONObject> _lstJSONOutput = new ArrayList<JSONObject>();
+    ArrayList<JSONObject> _lstJSONInput = new ArrayList<JSONObject>();
+    ArrayList<JSONObject> _lstJSONInputValues = new ArrayList<JSONObject>();
     ArrayList<JSONObject> _jsnTasks = new ArrayList<>();
+
 
 
     LMThread(JSONArray _jsnArgs) {
@@ -34,75 +39,79 @@ public class LMThread implements Runnable {
         }
     }
 
-
     private void parseArgs(JSONArray Args) {
         System.out.println("--------------------------------------");
         JSONArray commands = new JSONArray();
 
+        seperateIOC(Args);
+        matchIOC();
+        System.out.println("--------------------------------------");
+    }
 
-        for (int i = 0; i < Args.length(); i++) {
-            try {
-                JSONObject _jsnArgs = Args.getJSONObject(i);
-                if (_jsnArgs.has("outputgroup")) {
-                    //tests.add(_jsnArgs);
-                    _lstOutputSerials.add(_jsnArgs.getInt("outputgroup"));
+    private void matchIOC(){
+        /* get condition number for all outputs */
+        for(int i = 0; i < _lstJSONOutput.size(); i++){
+            JSONObject _jsnCurrentOutput = _lstJSONOutput.get(i);
+            JSONObject _jsnCurrentTask = new JSONObject();
+
+            _jsnCurrentTask.put("output", _jsnCurrentOutput.getInt("outputgroup"));
+            _jsnCurrentTask.put("conditionNumber", _jsnCurrentOutput.getInt("inputgroup"));
+            _jsnCurrentTask.put("operation", _jsnCurrentOutput.getInt("operation"));
+
+            _jsnTasks.add(_jsnCurrentTask);
+        }
+
+        /* for all conditions */
+        for(int j = 0; j < _jsnTasks.size(); j++){
+            JSONObject _jsnCurrentTask = _jsnTasks.get(j);
+            int currentOutputConditionGroup = _jsnCurrentTask.getInt("conditionNumber");
+            ArrayList<Integer> inputNumbers = new ArrayList<>();
+            ArrayList<Double> inputValues = new ArrayList<>();
+
+            /* find all associated inputs */
+            for (int k = 0; k < _lstJSONInput.size(); k++){
+                JSONObject _jsnCurrentInputItem = _lstJSONInput.get(k);
+                int _intCurrentItemCondGroup = _jsnCurrentInputItem.getInt("conditiongroup");
+                if( _intCurrentItemCondGroup == currentOutputConditionGroup){
+                    inputNumbers.add(_jsnCurrentInputItem.getInt("inputid"));
                 }
-                //System.out.print("line number" + i + ": ");
-                //System.out.println(_jsnArgs);
-            } catch (JSONException e) {
-                e.printStackTrace();
             }
-        }
 
-        /******** Will replace current function to seperate and match data
-        for (int i = 0; i < Args.length(); i++) {
-            //Extract outputs
-            //Extract inputs
-            //Match things
-        }
-        */
-
-        for (int j = 0; j < _lstOutputSerials.size();j++){
-            JSONObject _jsnItemOutput = Args.getJSONObject(j);
-            JSONObject _jsnNewCondition = new JSONObject();
-            ArrayList<Float> _strInputValues = new ArrayList<Float>();
-
-            //looking for outputgroup
-            if(_jsnItemOutput.has("outputgroup")){
-                if(_jsnItemOutput.getInt("outputgroup") == _lstOutputSerials.get(j)){
-                    _jsnNewCondition.put("outputgroup",_lstOutputSerials.get(j));
-                    _jsnNewCondition.put("operation", _jsnItemOutput.getInt("operation"));
-                    int _intInputGroupForCurrentOutput = _jsnItemOutput.getInt("inputgroup");
-                    System.out.println("Condition group for output " + _lstOutputSerials.get(j) + " is " + _intInputGroupForCurrentOutput);
-                    //find all inputs related
-                    for(int k = 0; k < Args.length(); k++){
-                        JSONObject _jsnItemInput = Args.getJSONObject(k);
-                        if(_jsnItemInput.has("conditiongroup")){
-                            //Find inputs with right conditiongroup
-                            if(_intInputGroupForCurrentOutput == _jsnItemInput.getInt("conditiongroup")){
-                                int _intWantedInputID = _jsnItemInput.getInt("inputid");
-                                System.out.println("Currently wanted inputid is: " + _intWantedInputID);
-                                for (int l = 0; l < Args.length(); l++){
-                                    JSONObject _jsnItemInputValues = Args.getJSONObject(l);
-                                    if(_jsnItemInputValues.has("serintinput")){
-                                        if(_jsnItemInputValues.getInt("serintinput") == _intWantedInputID){
-                                            //_jsnNewCondition.put("input", _jsnItemInputValues.getFloat("value"));
-                                            float _fltInputValue = _jsnItemInputValues.getFloat("value");
-                                            _strInputValues.add(_fltInputValue);
-                                        }
-                                    }
-                                }
-                            }
-                        }
+            /* find values of all inputs */
+            for (int l = 0; l < inputNumbers.size(); l++){
+                int _intCurrentInputNumber = inputNumbers.get(l);
+                for(int i = 0; i < _lstJSONInputValues.size(); i++){
+                    JSONObject _jsnCurrentItem = _lstJSONInputValues.get(i);
+                    if(_jsnCurrentItem.getInt("serintinput") == _intCurrentInputNumber){
+                        inputValues.add(_jsnCurrentItem.getDouble("value"));
+                        break;
                     }
                 }
             }
-            _jsnNewCondition.put("input",_strInputValues);
-            System.out.println(_jsnNewCondition);
-            _jsnTasks.add(_jsnNewCondition);
+            System.out.println("for output: " + _jsnCurrentTask.getInt("output") + " input ids are: " + inputNumbers);
+            System.out.println("for output: " + _jsnCurrentTask.getInt("output") + " input values are: " + inputValues);
+            _jsnCurrentTask.put("input", inputValues);
         }
-        
-        System.out.println("--------------------------------------");
+    }
+
+    private void seperateIOC(JSONArray Args){
+        System.out.println("seperating inputs/outputs/conds");
+        for (int i = 0; i < Args.length(); i++) {
+
+            JSONObject _jsnItem = Args.getJSONObject(i);
+
+            if(_jsnItem.has("outputgroup")){
+                _lstJSONOutput.add(Args.getJSONObject(i));
+            }
+
+            else if(_jsnItem.has("inputid")){
+                _lstJSONInput.add(Args.getJSONObject(i));
+            }
+
+            else {
+                _lstJSONInputValues.add(Args.getJSONObject(i));
+            }
+        }
     }
 
     private void processAllTasks(){
@@ -115,20 +124,73 @@ public class LMThread implements Runnable {
         int _intTaskID = _jsnTask.getInt("operation");
         switch(_intTaskID){
             case 0:
-                System.out.println("task ID is 0. Executing task division");
+                System.out.println("task ID is 0. Executing task greaterThan");
+                greaterThan(_jsnTask);
                 break;
             case 1:
-                System.out.println("task ID is 1. Executing task greaterThan");
+                System.out.println("task ID is 1. Executing task lessThan");
+                lessThan(_jsnTask);
                 break;
             case 2:
-                System.out.println("task ID is 2. Executing task lessThan");
+                System.out.println("task ID is 2. Executing task equalTo");
+                equalTo(_jsnTask);
             default:
                 System.out.println("task ID is not mapped to any behavior. Task ID received is : " + _intTaskID);
                 break;
         }
     }
 
-    private void updateOutput(int outputID, int value){
-        //send update to server
+    private void greaterThan(JSONObject _jsnTask){
+
+        double input1, input2;
+
+        JSONArray _jsnInputs = _jsnTask.getJSONArray("input");
+        input1 = _jsnInputs.getDouble(0);
+        input2 = _jsnInputs.getDouble(1);
+
+        System.out.println("GT test for values: " + input1 + " and " + input2);
+        if(input1 > input2){
+            System.out.println("input1 is greater than input2. PASSED");
+
+        }
+        else{
+            System.out.println("input1 is less than input2. FAILED");
+        }
+    }
+
+    private void lessThan(JSONObject _jsnTask){
+        double input1, input2;
+
+        JSONArray _jsnInputs = _jsnTask.getJSONArray("input");
+        input1 = _jsnInputs.getDouble(0);
+        input2 = _jsnInputs.getDouble(1);
+        System.out.println("LT test for values: " + input1 + " and " + input2);
+        if(input1 < input2){
+            System.out.println("input1 is less than input2. PASSED");
+
+        }
+        else{
+            System.out.println("input1 is greater than input2. FAILED");
+        }
+    }
+
+    private void equalTo(JSONObject _jsnTask){
+        double input1, input2;
+
+        JSONArray _jsnInputs = _jsnTask.getJSONArray("input");
+        input1 = _jsnInputs.getFloat(0);
+        input2 = _jsnInputs.getFloat(1);
+        System.out.println("ET test for values: " + input1 + " and " + input2);
+
+        double _dblUpperLimit = input1 * (1.0 + TOLERANCE);
+        double _dblLowerLimit = input1 * (1.0 - TOLERANCE);
+
+        if(input2 < _dblUpperLimit && input2 > _dblLowerLimit){
+            System.out.println("input1 is equal to input2. PASSED");
+        }
+
+        else{
+            System.out.println("input1 is not equal to input2. FAILED");
+        }
     }
 }
