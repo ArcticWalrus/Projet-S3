841940a98169316a65e5ecc54eed0d813dce9051
maxmamort <maximbolduc74@gmail.com>
2018-05-31 09:20:14 -0400

    added interface to persistantLayer

---
+++ src/maxmamort/gel/persistence/IpersistantLayer.java
@@ -0,0 +1,15 @@
+package maxmamort.gel.persistence;
+
+import org.json.JSONArray;
+
+public interface IpersistantLayer {
+    int addInput(String inputName, double defaultValue, int sensorType);
+
+    boolean updateOutputValue(int outputId, double value);
+
+    int createInputGroup(int[] inputIds);
+
+    int createCondition(int inputGroup, int outputGroup, int conditionType);
+
+    JSONArray getConditionsAndInputs(int inputId);
+}
