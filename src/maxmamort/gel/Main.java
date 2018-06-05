f0b43111ba0cbbbfa5f89a0965593f45476601af
ArcticWalrus <malcolm19@hotmail.com>
2018-06-05 10:24:07 -0400

    Application : quelque chose qui compile

--- src/maxmamort/gel/Main.java
+++ src/maxmamort/gel/Main.java
@@ -12,43 +12,56 @@ public class Main {
 
 
     public static void main(String[] args) {
-        persistantLayer pl = new persistantLayer();
 
-        int temp = pl.addInput("Test input", 22.5, INPUT);
+       ApplicationLayer al = new ApplicationLayer();
+       al.Populate();
+
+       JSONArray jsonFinal = al.pl.getConditionsAndInputs(al.temp);
+       al.Logic_Module_Parse(jsonFinal);
+
+
+
+
+        //Commented by Malcolm for test
+       // persistantLayer pl = new persistantLayer();
+
+       /* int temp = pl.addInput("Test input", 22.5, INPUT);
         int temp2 = pl.addInput("Input2", 21.2, INPUT);
         int inputGroup = pl.createInputGroup(new int[]{temp, temp2});
         int temp3 = pl.addInput("output1", 1.3, OUTPUT);
         int outputGroup = pl.createInputGroup(new int[]{temp3});
-        int conditionId = pl.createCondition(inputGroup, outputGroup, 1);
+        int conditionId = pl.createCondition(inputGroup, outputGroup, 1);*/
 
-        /* JOSH VALUES FOR LMTHREAD TESTS */
-        /* greaterThan test 1. Should pass */
+        //JOSH VALUES FOR LMTHREAD TESTS
+        //greaterThan test 1. Should pass
         //int gtTest1_value1 = pl.addInput("gtTest1_value1", 23.5, INPUT);
-        int gtTest1_value2 = pl.addInput("gtTest1_value2", 21.5, INPUT);
+        /*int gtTest1_value2 = pl.addInput("gtTest1_value2", 21.5, INPUT);
         int gtTest1_InputGroup = pl.createInputGroup(new int[]{temp, gtTest1_value2});
         int gtTest1_output = pl.addInput("gtTest1_output", 1.3, OUTPUT);
         int gtTest1_OutputGroup = pl.createInputGroup(new int[]{gtTest1_output});
-        int gtTest1_condID = pl.createCondition(gtTest1_InputGroup, gtTest1_OutputGroup, GREATERTHAN);
+        int gtTest1_condID = pl.createCondition(gtTest1_InputGroup, gtTest1_OutputGroup, GREATERTHAN);*/
 
-        /* lessthan test 1. Should pass */
+        // lessthan test 1. Should pass
         //int ltTest1_value1 = pl.addInput("ltTest1_value1", 23.5, INPUT);
-        int ltTest1_value2 = pl.addInput("gtTest1_value2", 23.5, INPUT);
+        /*int ltTest1_value2 = pl.addInput("gtTest1_value2", 23.5, INPUT);
         int ltTest1_InputGroup = pl.createInputGroup(new int[]{temp, ltTest1_value2});
         int ltTest1_output = pl.addInput("ltTest1_output", 1.3, OUTPUT);
         int ltTest1_OutputGroup = pl.createInputGroup(new int[]{ltTest1_output});
-        int ltTest1_condID = pl.createCondition(ltTest1_InputGroup, ltTest1_OutputGroup, LESSTHAN);
+        int ltTest1_condID = pl.createCondition(ltTest1_InputGroup, ltTest1_OutputGroup, LESSTHAN);*/
 
-        /* equal to test1. Should pass */
+        // equal to test1. Should pass
         //int etTest1_value1 = pl.addInput("etTest1_value1", 22.5, INPUT);
-        int etTest1_value2 = pl.addInput("etTest1_value2", 22.5, INPUT);
+        /*int etTest1_value2 = pl.addInput("etTest1_value2", 22.5, INPUT);
         int etTest1_InputGroup = pl.createInputGroup(new int[]{temp, etTest1_value2});
         int etTest1_output = pl.addInput("etTest1_output", 1.3, OUTPUT);
         int etTest1_OutputGroup = pl.createInputGroup(new int[]{etTest1_output});
-        int etTest1_condID = pl.createCondition(etTest1_InputGroup, etTest1_OutputGroup, EQUALTO);
+        int etTest1_condID = pl.createCondition(etTest1_InputGroup, etTest1_OutputGroup, EQUALTO);*/
 
 
-        JSONArray jsonFinal = pl.getConditionsAndInputs(temp);
-        LMThread _lmtLogic = new LMThread(jsonFinal);
+       // JSONArray jsonFinal = pl.getConditionsAndInputs(temp);
+        //LMThread _lmtLogic = new LMThread(jsonFinal);
+
+        //end comment malcolm
         /*
             for (int i = 0; i < jsonFinal.length(); i++) {
                 System.out.println(jsonFinal.get(i).toString());
