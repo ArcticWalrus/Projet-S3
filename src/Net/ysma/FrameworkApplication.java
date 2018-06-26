70812ac8314825360e16d49a0c4995145d9761cb
maxmamort <maximbolduc74@gmail.com>
2018-06-26 14:02:51 -0400

    stagged requested by ysma

--- src/Net/ysma/FrameworkApplication.java
+++ src/Net/ysma/FrameworkApplication.java
@@ -65,24 +65,83 @@ public class FrameworkApplication {
         _booAppOn = false;
     }
 
-    private void populate() {
-        SerialObj temp_obj = new SerialObj();
-        temp_obj.setDataFrame(new JSONArray().put(new JSONObject().put("inputName", "Test input").put("defaultValue", 22.5).put("sensorType", INPUT)));
-        temp_obj.setTargetType(PERSISTANCE);
-        temp_obj.setRequestType("addInput");
-
-        //TODO refractor
+    private SerialObj sendPersistance(SerialObj obj, boolean blocking) {
         CommClient cc = new CommClient("127.0.0.1", 45010);
-        cc.setSerialObject(temp_obj);
-        cc.setIfFeedbackNeeded(true);
+        cc.setSerialObject(obj);
         cc.start();
+        cc.setIfFeedbackNeeded(blocking);
         while (!cc.getIfDataReceived()) {
             Utils.sleep(10);
         }
-        temp_obj = cc.getInputBuffer();
         cc.setReceivedDataRead();
+        return cc.getInputBuffer();
+    }
+
+    private SerialObj sendPersistanceSetupObj(SerialObj obj, boolean blocking, String requestType) {
+        obj.setRequestType(requestType);
+        obj.setTargetType(PERSISTANCE);
+        return sendPersistance(obj, blocking);
+    }
+
+    private void populate() {
+        SerialObj temp_obj = new SerialObj();
+
+        temp_obj.setDataFrame(new JSONArray().put(new JSONObject().put("inputName", "Test input").put("defaultValue", 22.5).put("sensorType", INPUT)));
+        temp_obj = sendPersistanceSetupObj(temp_obj, true, "addInput");
+        int temp = temp_obj.getDataFrame().getJSONObject(0).getInt("serintinput");
 
         System.out.println(temp_obj.getDataFrame().toString());
-        System.out.println("Done populating");
+
+
+
+
+        /*
+          int temp = pl.addInput("Test input", 22.5, INPUT);
+        temp_test = temp; //for creation of LM thread in main
+        int temp2 = pl.addInput("Input2", 21.2, INPUT);
+        int temp3 = pl.addInput("output1", 1.3, OUTPUT);
+        pl.createInputGroupCondition(new int[]{temp, temp2, temp3}, 1);
+
+        //JOSH VALUES FOR LMTHREAD TESTS
+        //greaterThan test 1. Should pass
+        //int gtTest1_value1 = pl.addInput("gtTest1_value1", 23.5, INPUT);
+        int gtTest1_value2 = pl.addInput("gtTest1_value2", 21.5, INPUT);
+        int gtTest1_output = pl.addInput("gtTest1_output", 1.3, OUTPUT);
+        pl.createInputGroupCondition(new int[]{temp, gtTest1_value2, gtTest1_output}, 0);
+
+        // lessthan test 1. Should pass
+        //int ltTest1_value1 = pl.addInput("ltTest1_value1", 23.5, INPUT);
+        int ltTest1_value2 = pl.addInput("gtTest1_value2", 23.5, INPUT);
+        int ltTest1_output = pl.addInput("ltTest1_output", 1.3, OUTPUT);
+        pl.createInputGroupCondition(new int[]{temp, ltTest1_value2, ltTest1_output}, 2);
+
+        // equal to test1. Should pass
+        //int etTest1_value1 = pl.addInput("etTest1_value1", 22.5, INPUT);
+        int etTest1_value2 = pl.addInput("etTest1_value2", 22.5, INPUT);
+        int etTest1_output = pl.addInput("etTest1_output", 1.3, OUTPUT);
+        pl.createInputGroupCondition(new int[]{temp, etTest1_value2, etTest1_output}, 1);
+
+        pl.createUser("bolm2210");
+        pl.createUser("denj1605");
+        pl.createUser("demo1622");
+
+        System.out.println("\n\n Creating Devices");
+        pl.createDevice("THIS IS A MAC", "bolm2210", "name of device");
+        pl.createDevice("MAC", "bolm2210", "name of device 0");
+        pl.createDevice("THIS IS A MAC 2", "bolm2210", "name of device 2");
+        pl.createDevice("THIS IS A MAC 3", "denj1605", "name of device 3");
+
+        pl.renameDevice("MAC", "new name");
+        pl.updateDeviceIP("MAC", "1.2.3.4");
+
+
+        // Not working cause of DB uniquenesss constraint
+        pl.createIO("name of IO 1", "MAC", 10, 1);
+        pl.createIO("name of IO 2", "MAC", 11, 0);
+        pl.createIO("name of IO 3", "MAC", 12, 1);
+
+        JSONArray deviceID = pl.getIOByDevice("MAC");
+        System.out.println("IO for device MAC: " + deviceID.toString());
+         */
     }
 }
